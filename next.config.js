8d01894752b0eef0f8f62dcd7e76abe2c90ec91f
Valerii <valerii.lemberov@applyft.co>
2026-06-02 18:24:07 +0300

    fix: increase body size limit to 10mb for fix requests

--- next.config.js
+++ next.config.js
@@ -2,13 +2,18 @@
 const nextConfig = {
   images: {
     domains: [
-      'lh3.googleusercontent.com', // Google аватарки
-      'res.cloudinary.com',         // Cloudinary
+      'lh3.googleusercontent.com',
+      'res.cloudinary.com',
     ],
   },
   experimental: {
     serverComponentsExternalPackages: ['sharp'],
   },
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb',
+    },
+  },
 }
-
+ 
 module.exports = nextConfig
